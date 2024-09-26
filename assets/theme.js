2d0142eb399cbd06b1bf352664217248582c6c4a
Arocep Federal <rick@bumblinbee.com>
2024-09-26 12:51:39 -0500

    theme.js

--- assets/theme.js
+++ assets/theme.js
@@ -252,6 +252,24 @@ if (console && console.log) {
   });
   
   const trapFocusHandlers = {};
+
+  $('.button_super_inner').mouseenter(function (e) {
+    var parentOffset = $(this).offset();
+    var relX = e.pageX - parentOffset.left;
+    var relY = e.pageY - parentOffset.top;
+    $(this).prev('.super_button_circle').css({ left: relX, top: relY });
+    $(this).prev('.super_button_circle').removeClass('desplode-circle');
+    $(this).prev('.super_button_circle').addClass('explode-circle');
+  });
+
+  $('.button_super_inner').mouseleave(function (e) {
+    var parentOffset = $(this).offset();
+    var relX = e.pageX - parentOffset.left;
+    var relY = e.pageY - parentOffset.top;
+    $(this).prev('.super_button_circle').css({ left: relX, top: relY });
+    $(this).prev('.super_button_circle').removeClass('explode-circle');
+    $(this).prev('.super_button_circle').addClass('desplode-circle');
+  });
   
   /**
    * Currency Helpers
